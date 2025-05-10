46ed807c2273562a242a88567ff0ae4482ddc0e9
sintom <siniak.tomasz@gmail.com>
2025-05-10 13:54:49 +0200

    fix buiild image step 2

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildApp() {
 
 def buildImage() {
   echo "Building docker image and push to docker hub..."
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', userVariable: 'USER')]) {
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     sh 'docker build -t yom/demo-app:1.0'
     sh 'echo $PASS | docker login -u $USER --password-stdin'
     sh 'docker push'
